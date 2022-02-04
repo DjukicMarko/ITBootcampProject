483d7aaf632548e49f66dbbc143d414caa7e3f99
DjukicMarko <ortak.niko@gmail.com>
2022-02-04 19:45:38 +0100

    upload project

--- src/test/java/TestPage/TestLetcodeYoutube.java
+++ src/test/java/TestPage/TestLetcodeYoutube.java
@@ -4,6 +4,7 @@ import Page.LetcodeYoutube;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
@@ -31,5 +32,6 @@ public class TestLetcodeYoutube {
         wd.get("https://letcode.in/");
         LetcodeYoutube lcy = new LetcodeYoutube(wd);
         lcy.youTubeButton();
+        Assert.assertTrue(wd.getCurrentUrl().equals("https://accounts.google.com/signin/v2/identifier?service=youtube&uilel=3&passive=true&continue=https%3A%2F%2Fwww.youtube.com%2Fsignin%3Faction_handle_signin%3Dtrue%26app%3Ddesktop%26hl%3Den%26next%3D%252Fchannel%252FUCNcnqL0P17hISKlOxTjkJ0g%253Fsub_confirmation%253D1%2526feature%253Dsubscribe-embed-click%26feature%3Dsubscribe&hl=en&flowName=GlifWebSignIn&flowEntry=ServiceLogin"), "The URL is correct.");
     }
 }
